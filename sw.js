6e50aeb79222deaf805d172effafd68a00ad48f1
Jorge Armando Ayala <armandolnayala@gmail.com>
2019-07-26 16:13:01 -0300

    Corrijo forma de ver en IOS y agrego logica para borrar cache dynamic

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE    ='static-v3';
-const DYNAMIC_CACHE   ='dynamic-v1';
+const STATIC_CACHE    ='static-v4';
+const DYNAMIC_CACHE   ='dynamic-v2';
 const INMUTABLE_CACHE ='inmutable-v1';
 
 const APP_SHELL=[
@@ -42,8 +42,15 @@ self.addEventListener('activate',e=>{
   //Borro todos los caches static-n donde n no sea el actual
   const respuesta= caches.keys().then(keys=>{
     keys.forEach(key=>{
+      
       if(key !== STATIC_CACHE
         && key.includes('static'))
+      {
+        return caches.delete(key);
+      }
+
+      if(key !== DYNAMIC_CACHE
+        && key.includes('dynamic'))
       {
         return caches.delete(key);
       }
